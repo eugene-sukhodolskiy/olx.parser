b63bec244be0ec516c1a5efaf491e78c83186a38
Unknown <oleggordiichenko@gmail.com>
2019-03-18 13:02:22 +0200

    first ver of parsed JSON is created

--- Parser.py
+++ Parser.py
@@ -1,4 +1,6 @@
 import requests
+import json
+
 
 class Parser:
 	_keyWord = None
@@ -18,16 +20,81 @@ class Parser:
 		return greetings[:-1]
 
 	def resp(self):
+		keysList = ["photo", "title", "page", "price"]
+		resDict = {}
+		resDictList = []
+		numberOfElem = 0
 		response = requests.get('https://www.olx.ua//list/q-samsung/')
 		a = str(response.content)
 		b = a.split("class=\"wrap\"")
+		resStr = []  # img, link, title, price
 		for i in b:
 			l = i.split(">\\n")
+			# l = i
 			for k in l:
+				imgUrl = None
+				link = None
+				title = None
+				price = None
+
 				subStr = "<img class=\"fleft\" src=\""
 				begSrc = k.find(subStr)
+				endSrc = 0
+
 				if begSrc != (-1):
 					begSrc+=len(subStr)
 					endSrc = k.find("\"", begSrc + 1)
-					# print(k[begSrc : endSrc])
-					# print("___________________________")
+					imgUrl = k[begSrc : endSrc]
+					# print("imgUrl:", k[begSrc : endSrc])  # img url
+					resStr.append(imgUrl)
+					
+				begLink = k.find("<a href=\"https://www.olx.ua/obyavlenie/")
+				if begLink != (-1):
+					begLink = k.find("https://")
+					endLink = k.find("\"", begLink + len("https://"))
+					link = k[begLink : endLink]
+					# print("link:", k[begLink : endLink])
+					resStr.append(link)
+
+				begTitle = k.find("<strong>")
+				if begTitle != (-1):
+					tmpLen = len("<strong>")
+					title = k[begTitle + tmpLen : len(k) - tmpLen]
+					# print("title:", k[begTitle + tmpLen : len(k) - tmpLen])  # img url
+					# print("_________________________")
+					resStr.append(title)
+
+
+				if imgUrl is None and link is None and title is not None:
+					endPrice = title.find("\\xd0\\xb3\\xd1\\x80\\xd0\\xbd.")
+					if endPrice != (-1):
+						tmpStr = title[:endPrice]
+						tmpStr1 = tmpStr.replace(' ', '')
+						price = int(tmpStr1)
+						# print(resStr)
+						if len(resStr) == 4:
+							resStr[3] = price
+							# print(resStr)
+
+							# create dict
+							for i in range(len(resStr)):
+								resDict.update({ keysList[i]: resStr[i]})
+							resDictList.append(dict(resDict))  # hard copy of dict obj
+							numberOfElem+=1
+						else:
+							resStr.clear()
+
+		# print(self.toJson({"result": resDictList}))
+
+		return self.toJson({"result": resDictList})
+
+	def toJson(self, data = None):
+		if data is not None:
+			# data = {}
+			# data['key'] = 'value'
+			json_data = json.dumps(data)
+		else:
+			print("toJson:", "Error: wrong data!")
+
+		return json_data
+						
--- routes/APIRoutes.py
+++ routes/APIRoutes.py
@@ -2,11 +2,11 @@ from Parser import Parser
 from flask import request
 
 def APIRoutes(app):
-
+	# http://127.0.0.1:5000/rest-api/v1/search?word=125
 	@app.route("/rest-api/v1/search", methods=['GET'])
 	def search():
 		P = Parser()
 		word = request.args.get('word')		
 		P.keyWord(word)
-		P.resp()
+		print(P.resp())  # yours JSON
 		return P.hi()
