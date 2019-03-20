53c097f713368bfc701ddd8f585468c93c331bfa
Unknown <oleggordiichenko@gmail.com>
2019-03-20 13:38:04 +0200

    parser is working again. Json unicode bug!

--- Parser.py
+++ Parser.py
@@ -8,7 +8,7 @@ class Parser:
 		pass
 
 	def resp(self, word):
-		keysList = ["photo", "page", "title", "price"]
+		keysList = ["photo", "title", "page", "price"]
 		resDict = {}
 		resDictList = []
 		numberOfElem = 0
@@ -17,58 +17,59 @@ class Parser:
 		b = a.split("class=\"wrap\"")
 		resStr = []  # img, link, title, price
 		for i in b:
-			l = i.split(">\\n")
-			# l = i
+			l = i.split("<")
+
 			for k in l:
 				imgUrl = None
 				link = None
 				title = None
 				price = None
 
-				subStr = "<img class=\"fleft\" src=\""
+				subStr = "img class=\"fleft\" src=\""
+				subStr2 = " alt=\""
+
 				begSrc = k.find(subStr)
-				endSrc = 0
+				begSrc2 = k.find(subStr2)
+				# endSrc = 0
 
 				if begSrc != (-1):
 					begSrc+=len(subStr)
-					endSrc = k.find("\"", begSrc + 1)
+					begSrc2+=len(subStr2)
+					endSrc = k.find(";s=261x203", begSrc + 1)
+					endSrc2 = k.find("\">", begSrc2 + 1)
 					imgUrl = k[begSrc : endSrc]
+					title = k[begSrc2 : endSrc2]
+					# print("imgUrl:", imgUrl)
+					# print("title:", title)
 					resStr.append(imgUrl) # img url
+					resStr.append(title) # img url
 					
-				begLink = k.find("<a href=\"https://www.olx.ua/obyavlenie/")
+				begLink = k.find("a href=\"https://www.olx.ua/obyavlenie/")
 				if begLink != (-1):
 					begLink = k.find("https://")
-					endLink = k.find("\"", begLink + len("https://"))
+					endLink = k.find(".html", begLink + len("https://")) + 5  # 5 len(".html")
 					link = k[begLink : endLink]
+					# print("link:", link)
 					resStr.append(link)  # link
 
-				begTitle = k.find("<strong>")
-				if begTitle != (-1):
-					tmpLen = len("<strong>")
-					title = k[begTitle + tmpLen : len(k) - tmpLen]
-					resStr.append(title)  # title or price
+				begPrice = k.find("strong>")
+				# print("begPrice", begPrice)
+				if begPrice != (-1) and k[-1] == '.':
+					tmpLen = len("strong>")
+					price = k[tmpLen : len(k) - 1]
+					# print("price:", price)
+					resStr.append(price)  # title or price
 
+			if len(resStr) == 4:
+				# create dict
+				for i in range(len(resStr)):
+					resDict.update({ keysList[i]: resStr[i]})
+				resDictList.append(dict(resDict))  # hard copy of dict obj
+				resStr.clear()
 
-				if imgUrl is None and link is None and title is not None:
-					endPrice = title.find("грн.")
-					if endPrice != (-1):
-						tmpStr = title[:endPrice]
-						tmpStr1 = tmpStr.replace(' ', '')
-						price = float(tmpStr1)  # formated price
-
-						if len(resStr) == 4:
-							resStr[3] = price
-
-							# create dict
-							for i in range(len(resStr)):
-								resDict.update({ keysList[i]: resStr[i]})
-							resDictList.append(dict(resDict))  # hard copy of dict obj
-							numberOfElem+=1
-						else:
-							resStr.clear()
-
-		# print(self.toJson({"result": resDictList}))
+		print(self.toJson({"result": resDictList}))
 		return self.toJson({"result": resDictList})
+		# return "test"
 
 	def toJson(self, data = None):
 		if data is not None:
