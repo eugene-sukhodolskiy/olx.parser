30b23b0271e02d3e9509ea10652c062d40093324
s_eugene_r <s_eugene_r@yahoo.com>
2019-03-22 12:55:45 +0200

    shutdown testcode for AjaxQuery

--- routes/APIRoutes.py
+++ routes/APIRoutes.py
@@ -2,12 +2,10 @@ from Parser import Parser
 from flask import request
 from AltParser import AltParser
 import json
-from entity.AjaxQuery import AjaxQuery
 
 def APIRoutes(app):
-	@app.route("/rest-api/v1/search", methods = ['GET'])
+	@app.route("/rest-api/v0/search", methods = ['GET'])
 	def search():
-		aq = AjaxQuery()
 		P = Parser()
 		query = request.args.get('query')	
 		JSON_from_Parcer = P.search(query)
@@ -18,7 +16,7 @@ def APIRoutes(app):
 		return JSON_from_Parcer # yours JSON
 		pass
 
-	@app.route("/rest-api/v0/search", methods = ['GET'])
+	@app.route("/rest-api/v1/search", methods = ['GET'])
 	def _search():
 		altparser = AltParser(query = request.args.get('query'))
 		products = altparser.get_prods_from_page()
