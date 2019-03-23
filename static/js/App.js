b8774506a98a4171d5a4a4868adf72484d4bb5a4
s_eugene_r <s_eugene_r@yahoo.com>
2019-03-23 14:58:21 +0200

    Load search with load page

--- static/js/App.js
+++ static/js/App.js
@@ -5,6 +5,12 @@ class Application{
 		this.data = {result: [{title: "Title", price: "price", currency: "currency", is_exchange: "is_exchange", is_promoted: "is_promoted", url: "url_to_page", thumb: "photo"}]};
 		// this.data = {result: [{title: "Title", price: "price", currency: "currency", url: "url_to_page", thumb: "photo"}]};
 		this.ssr = new SSRender('.result-container', this.data);
+		if(document.location.hash != ''){
+			const query = document.location.hash.split("#")[1];
+			ss('.search-field .control').toggleClass('is-loading');
+			ss('.search-field input').first().value = query;
+			this.searching(query);
+		}
 		this.searchFieldListener();
 	}
 
@@ -18,7 +24,9 @@ class Application{
 		ss('.search-field input').on('keydown', function(e){
 			if(e.code == 'Enter'){
 				ss('.search-field .control').toggleClass('is-loading');
-				self.searching(this.value.trim());
+				let query = this.value.trim();
+				document.location.hash = query;
+				self.searching(query);
 			}
 		});
 	}
